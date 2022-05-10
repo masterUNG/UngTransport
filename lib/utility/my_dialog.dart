e629a5032847841177d02ffb679ffc2984dc6388
masterUNG <phrombutr@gmail.com>
2022-05-10 15:19:41 +0700

    update

--- lib/utility/my_dialog.dart
+++ lib/utility/my_dialog.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:ungtransport/utility/my_constant.dart';
 import 'package:ungtransport/widges/show_image.dart';
+import 'package:ungtransport/widges/show_text.dart';
+import 'package:ungtransport/widges/show_text_button.dart';
 
 class MyDialog {
   final BuildContext context;
@@ -14,12 +17,38 @@ class MyDialog {
       context: context,
       builder: (BuildContext context) => AlertDialog(
         title: ListTile(
-          leading: SizedBox(width: 100,
+          leading: const SizedBox(
+            width: 80,
             child: ShowImage(
               path: 'images/avatar.png',
             ),
           ),
+          title: ShowText(
+            label: title,
+            textStyle: MyConstant().h2Style(),
+          ),
+          subtitle: ShowText(label: subTitle),
         ),
+        actions: [
+          ShowTextButton(
+            label: 'Camera',
+            pressFunc: () {
+               Navigator.pop(context);
+            },
+          ),
+          ShowTextButton(
+            label: 'Gallery',
+            pressFunc: () {
+               Navigator.pop(context);
+            },
+          ),
+          ShowTextButton(
+            label: 'Cancel',
+            pressFunc: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
