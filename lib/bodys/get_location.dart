d4359454451671c76279d0f17b561abfa206635c
masterUNG <phrombutr@gmail.com>
2022-05-12 15:49:49 +0700

    Get Location Success

--- lib/bodys/get_location.dart
+++ lib/bodys/get_location.dart
@@ -1,12 +1,84 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class GetLocationShowMap extends StatelessWidget {
-  const GetLocationShowMap({ Key? key }) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:ungtransport/utility/my_dialog.dart';
+
+class GetLocationShowMap extends StatefulWidget {
+  const GetLocationShowMap({Key? key}) : super(key: key);
+
+  @override
+  State<GetLocationShowMap> createState() => _GetLocationShowMapState();
+}
+
+class _GetLocationShowMapState extends State<GetLocationShowMap> {
+  double? lat, lng;
+  bool load = true;
+
+  @override
+  void initState() {
+    super.initState();
+    findLatLng();
+  }
+
+  Future<void> findLatLng() async {
+    bool locationEnable;
+    LocationPermission locationPermission;
+
+    locationEnable = await Geolocator.isLocationServiceEnabled();
+    if (locationEnable) {
+      // Enablen Location Service
+      locationPermission = await Geolocator.checkPermission();
+      if (locationPermission == LocationPermission.deniedForever) {
+        MyDialog(context: context).normalDialog(
+            title: 'Denyed Forever',
+            subTitle: 'Please Open Premission',
+            pressFunc: () {
+              exit(0);
+            });
+      }
+
+      if (locationPermission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      } else {
+        // Alway
+        print('Location Can Find');
+
+        Position position = await findPosition();
+        if (position != null) {
+          lat = position.latitude;
+          lng = position.longitude;
+          print('lat = $lat, lng = $lng');
+          load = false;
+          setState(() {});
+        }
+      }
+    } else {
+      MyDialog(context: context).normalDialog(
+          title: 'Non Location Service',
+          subTitle: 'Please Open Service',
+          pressFunc: () {
+            exit(0);
+          });
+    }
+  }
+
+  Future<Position> findPosition() async {
+    Position? position;
+    try {
+      position = await Geolocator.getCurrentPosition();
+    } catch (e) {
+      position = null;
+    }
+    return position!;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('This is GetLocation'),
+      body: load
+          ? const Center(child: CircularProgressIndicator())
+          : Text('This is GetLocation \n lat = $lat, lng = $lng'),
     );
   }
 }
